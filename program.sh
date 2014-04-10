69bee31dbc50f067dc5709160436c0979212236a
NexuS-Pt <geral@nexus-pt.eu>
2014-04-10 17:51:58 +0100

    add email system, doesn't work in my network

--- program.sh
+++ program.sh
@@ -16,12 +16,29 @@ rsync -avz --backup-dir=DIR $SOURCE $DEST"$4$3$2"
 echo
 
 # retornar após 1 min
-find $DEST -maxdepth 1 -type d -mmin +1 -exec echo {} \;
+#find $DEST -maxdepth 1 -type d -mmin +1 -exec echo {} \;
 
 # apagar após 60 dias
-find $DEST -maxdepth 1 -type d -mtime +60 -exec rm -rf {} \;
+#find $DEST -maxdepth 1 -type d -mtime +30 -exec rm -rf {} \;
 
 echo
 echo "------------------------------------------------------"
 echo
 echo
+
+
+#(
+#	echo helo incograf.com;
+#	sleep 2;
+#	echo mail from:carlossantos@incograf.com;
+#	sleep 2;
+#	echo rcpt to:carlossantos@incograf.net;
+#	sleep 2;
+#	echo data;
+#	sleep 2;
+#	echo subject: subject;
+#	sleep 2;
+#	echo .;
+#	sleep 2;
+#	echo exit
+#) | telnet incograf.com 25
